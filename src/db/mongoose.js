bde7d12d3ed8d02348d310e902b2136c67e488eb
Katleho <akhanye@student.wethinkcode.co.za>
2022-05-03 20:45:01 +0200

    Initializing the backend, no work for sc still.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
+const region = 'rsa';
+let mongo_url = `${process.env.MONGODB_URL_PREFIX}${region}${process.env.MONGODB_URL_SUFFIX}`
+mongoose.connect(mongo_url, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 }).then(() => {
